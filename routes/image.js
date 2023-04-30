c2969a14348802cdcbea77145d61d8b40cc6b68f
tengxong <119726458+tengxong@users.noreply.github.com>
2023-04-30 15:20:36 +0700

    back3

--- routes/image.js
+++ routes/image.js
@@ -1,6 +1,22 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const multer = require('multer');
+// const internal = require('stream');
+// const { Server } = require('http');
+
+const storage = multer.diskStorage({
+      destination: function(req,file,cb){
+            cb(null,'images');
+      },
+      filename: function (req, file,cb){
+            const ext =path.extname(file.originalname);
+            cb(null, Date.now() + ext);
+      }
+});
+
+
+const upload = multer({storage});
 
 // Serve image files
 app.get('/images/:imageName', (req, res) => {
@@ -12,6 +28,14 @@ app.get('/images/:imageName', (req, res) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
+});
+app.post('/uploadProfileImage',upload.single('image'), (req,res) =>{
+      try{
+            res.send('Image uploaded');
+      } catch(error){
+            console.error(error)
+            res.status(500).send('internal Server Error');
+      }
 });
 
 module.exports = app;
