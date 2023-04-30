9bafb2b5d2fd99fd221c3bfc531309e32fecd43f
tengxong <119726458+tengxong@users.noreply.github.com>
2023-04-30 14:10:29 +0700

    back2

--- routes/image.js
+++ routes/image.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-app.get ('/images/:imageName',(req,res) =>{
-      try {
-      const{imageName} = req.params
-      res.sendFile(imageName,{root:'./images'})
-      } catch(e){
-            console.log(e)
-      }
-})
-module.exports=app
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Serve image files
+app.get('/images/:imageName', (req, res) => {
+  try {
+    const imageName = req.params.imageName;
+    const imagePath = path.join(__dirname, '..', 'images', imageName);
+    res.sendFile(imagePath);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = app;
